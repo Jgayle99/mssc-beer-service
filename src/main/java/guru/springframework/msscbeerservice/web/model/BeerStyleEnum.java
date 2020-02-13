6f2b491c911512b3c530d494198051b4a1aa5e99
Jgayle <Jgayle99@gmail.com>
2020-02-13 07:03:24 -0500

    Adding web data model. Closes #2

---
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,8 @@
+package guru.springframework.msscbeerservice.web.model;
+
+/**
+ * Created by Joel Gayle on 13 Feb, 2020
+ */
+public enum BeerStyleEnum {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
